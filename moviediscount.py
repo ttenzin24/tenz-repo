e4a642aff2bfde91ae262239d4221fc77b3f34e3
Sangay Tenzin <02230271.cst@rub.edu.bt>
2024-03-01 22:29:13 +0600

    yyy

--- moviediscount.py
+++ moviediscount.py
@@ -1,13 +1,11 @@
 x = input("Enter your age:")
-print(x)
 
 y = input("Are you a student? (yes/no):")
-print(y)
 
 if x <= "12" :
     print("You are eligible for a discount on the movie ticket!")
 
-elif x >= "13"and x <= "18" and y == "yes" :
+elif (x >= "13" or x <= "18") and y == "yes" :
     print("You are eligible for a discount on movie ticket!")
 
 else:
