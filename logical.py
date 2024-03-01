3f09aab94019884ceae85d7c3d33620c392951f2
Sangay Tenzin <02230271.cst@rub.edu.bt>
2024-03-01 13:56:43 +0600

    logical.py changes

---
+++ logical.py
@@ -0,0 +1,17 @@
+#and operator
+x = 5
+print(x > 3 and x < 10)
+y = 12
+print(y > 10 and y % 5 == 0)
+
+#or operator
+x = 5 
+print(x < 3 or x > 10)
+y = 12
+print(y < 10 or y % 2 == 0)
+
+#NOT opertor
+x = 5
+print(not(x > 3 and x < 10) )
+y = 12
+print(not(y > 10 and y % 5 == 0))
