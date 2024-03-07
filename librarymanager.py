a09c7e7d46720dfa886419c52d2d127a5468dc8c
Sangay Tenzin <02230271.cst@rub.edu.bt>
2024-03-07 14:07:23 +0600

    librarymanager.py changes

---
+++ librarymanager.py
@@ -0,0 +1,41 @@
+# Initialize empty lists, set, and dictionary
+books_list = []
+authors_set = set()
+books_dict = {}
+
+# Add books
+books_list.append("Python Programming")
+authors_set.add("John Smith")
+books_dict["Python Programming"] = "John Smith"
+
+books_list.append("Data Structure and Algorithms")
+authors_set.add("Jane Doe")
+books_dict["Data Structure and Algorithms"] = "Jane Doe"
+
+books_list.append("Machine Learning Basics")
+authors_set.add("Alice Johnson")
+books_dict["Machine Learning Basics"] = "Alice Johnson"
+
+# Search for a book
+search_title = input("Enter the title of the book to search:")
+if search_title in books_list:
+    print(f"Book found! Author: {books_dict[search_title]}")
+else:
+    print("Book not found!")
+
+# Display all books
+print("List of Books:")
+for book in books_list:
+    print(book)
+
+# Remove Book
+remove_title = input("Enter the title of t he book to remove or else enter the skip:")
+if remove_title in books_list:
+    remove_author = books_dict[remove_title]
+    books_list.remove(remove_title)
+    authors_set.remove(remove_author)
+    del books_dict[remove_title]
+    print("Books removed successfully!")
+    print("Books available: ", books_list)
+else:
+    print("Book not found!")
