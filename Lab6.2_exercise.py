2de7a84df8b4f22b85bf15a685fb6853e0888615
Sangay Tenzin <02230271.cst@rub.edu.bt>
2024-05-27 17:02:52 +0600

    Right triangle pattern

---
+++ Lab6.2_exercise.py
@@ -0,0 +1,6 @@
+height = int(input("Enter the height of the triangle (number of rows): "))
+
+# Print the pattern of stars
+print("Pattern of stars in the shape of a right triangle:")
+for i in range(1, height + 1):
+    print("*" * i)
