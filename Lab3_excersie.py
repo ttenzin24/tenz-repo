19cc909f63621438818ace20722a4183d46481a7
Sangay Tenzin <02230271.cst@rub.edu.bt>
2024-05-27 16:47:06 +0600

    Discount using logical

---
+++ Lab3_excersie.py
@@ -0,0 +1,10 @@
+age = int(input("Enter your age: "))
+
+student = input("Are you a student? (yes/no): ")
+
+discount = (age <= 12) or (13 <= age <= 18 and student == "yes")
+
+if discount:
+    print("You are eligible for a discount.")
+else:
+    print("You are not eligible for a discount.")
